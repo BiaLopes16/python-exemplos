60a35cf286f88ab1df4e11a08b8abdba6af2343f
BiaLopes16 <anabeatrizlopesdesouza07@gmail.com>
2024-04-23 16:44:18 -0300

    Criação Variáveis.py

---
+++ Variáveis.py
@@ -0,0 +1,12 @@
+Nome="Bibia Lopes"
+Idade=16
+Email='anabeatrizlopesdesouza07@gmail.com'
+Telefone='17 99635-6010'
+Nota=9.99
+Aprovado=True
+print(Nome)
+print(Idade)
+print(Telefone)
+print(Nota)
+print(Aprovado)
+
