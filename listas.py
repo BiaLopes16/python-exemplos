66ef4afc4bd589caa8d79d13c05391ad6ee8d117
BiaLopes16 <anabeatrizlopesdesouza07@gmail.com>
2024-05-21 14:59:40 -0300

    Criação listas.py

---
+++ listas.py
@@ -0,0 +1,9 @@
+aluno =["Bia Lopes", 4, 3,5,False]
+print(f"Nome: {aluno[0]}")
+print(f"Faltas: {aluno[1]}")
+print(f"Média: {aluno[2]}")
+if aluno[3] == True:
+    print(f"Aprovado: Sim")
+else:
+    print(f"Aprovado: Não")
+
